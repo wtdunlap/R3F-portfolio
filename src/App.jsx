4c4a4bb1656efa62a8679710400d7791b7f220b4
834R807 <150474122+wtdunlap@users.noreply.github.com>
2024-10-15 12:51:56 -0400

    added performance metrics for better testing

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import AboutMe from "./assets/AboutMe.jsx";
 import ContactMe from "./assets/ContactMe.jsx";
 import Intro from "./assets/Intro.jsx";
 
+import { Stats } from "@react-three/drei";
+
 function App() {
     return (
         <>
             <Intro />
             <AboutMe />
             <ContactMe />
+            <Stats />
         </>
     );
 }
